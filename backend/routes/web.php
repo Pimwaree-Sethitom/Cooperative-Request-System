469d227c2f567e354b0682ee29452b5a1701f9a0
Pimwaree-Sethitom <pimwaree9072@gmail.com>
2026-04-24 14:28:46 +0700

    fix: postman collection download button

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -5,3 +5,13 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/download-postman', function () {
+    $file = public_path('postman/cooperative_api.json');
+    if (file_exists($file)) {
+        return response()->download($file, 'cooperative_api.json', [
+            'Content-Type' => 'application/json',
+        ]);
+    }
+    abort(404);
+})->name('download.postman');
